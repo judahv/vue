25aab5914c387c96df132489183c8921ac6541e3
judahv <load_kamelot@yahoo.co.jp>
2018-07-21 11:18:42 +0900

    参考文献追加

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -98,4 +98,5 @@ new Vue({
 /*
  * 【疑問】:class="{is-error:isError}" とクラス名を「is-error」にしたいが、ならない。「error」ならＯＫみたい
  * 参考文献：localStorage　https://qiita.com/shingorow/items/97c265d4cab33cb13b6c
+ * 配列から特定のものを削除　https://qiita.com/_shimizu/items/b8eac14f399e20599818
  */
